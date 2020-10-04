1842575c188c7b91848d74f99bc6648fe2b4f501
mjd119 <mjd119@pitt.edu>
2020-10-04 13:12:31 -0400

    Remove placeholder code

--- Cloud_Data_Storage.py
+++ Cloud_Data_Storage.py
@@ -72,35 +72,3 @@ response = table.get_item(
 item = response['Item']
 print(item)
 response
-
-
-
-# Upload a file, 'testt.jpg' into the newly created bucket
-s3.Object('datacont-hw3', 'test.jpg').put(Body=open('/home/mydata/test.jpg', 'rb'))
-
-dyndb = boto3.resource('dynamodb', region_name='us-west-2')
-
-# The first time that we define a table, we use
-table = dyndb.create_table(
-    TableName = 'DataTable',
-    KeySchema = [
-        { 'AttributeName': 'PartitionKey', 'KeyType': 'HASH' },
-        { 'AttributeName': 'RowKey', 'AttributeType': 'S' }
-    ]
-)
-# Wait for the table to be created
-table.meta.client.get_waiter('table_exists').wait(TableName='DataTable')
-
-# If the table has been previously defined, use:
-# table = dyndb.Table("DataTable")
-
-urlbase = "https://s3-us-west-2.amazonaws.com/datacont-hw3/"
-with open('\path-to-your-data\experiments.csv', 'rb') as csvfile:
-    csvf = csv.reader(csvfile, delimiter=',', quotechar='|')
-    for item in csvf:
-        body = open('path-to-your-data\datafiles\\'+item[3], 'rb')
-        md = s3.Object('datacont-hw3', item[3].Acl()).put(ACL='public-read')
-        url=urlbase+item[3]
-        metadat_item={'PartitionKey' : item[0], 'RowKey' : item[1],
-                      'description' : item[4], 'date' : item[2], 'url' : url}
-        table.put_item(Item=metadata_item)
